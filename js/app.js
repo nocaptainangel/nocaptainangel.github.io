526f75aed7789b8fc9f4b4a7ea9c09b1cb0c46e7
Angel <angel.leijendekker@gmail.com>
2018-11-19 14:42:07 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    // Banner words wrapper
+    var wordsWrapper = $('.banner .words-wrapper');
+    var words = wordsWrapper.find('b');
+    var wordDelay = 1000;
+    var wordIndex = 0;
+
+    // Full page of sections
+    $('#fullpage').fullpage({
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE'
+    });
+
+    // Animate the banner words
+    animateBannerWords();
+
+    function animateBannerWords() {
+        setTimeout(function () {
+            wordsWrapper.animate({
+                'width': 0
+            },
+            wordDelay,
+            'swing',
+            function () {
+                var current = $(words[wordIndex]);
+                current.addClass('hidden', '');
+
+                if (wordIndex === words.length - 1) {
+                    wordIndex = 0;
+                } else {
+                    wordIndex++;
+                }
+
+                var next = $(words[wordIndex]);
+                next.removeClass('hidden');
+
+                wordsWrapper.animate({
+                    'width': next.width()
+                },
+                wordDelay,
+                'swing',
+                function () {
+                    animateBannerWords();
+                });
+            });
+        }, 2000);
+    }
+
+})();
