6a21b1a41aa35e5655510405435798511eb09999
Angel <angel.leijendekker@gmail.com>
2019-04-30 01:45:34 +0800

    added section fade js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-(function () {
+$(document).ready(function () {
     'use strict';
+
+    // Full page of sections
+    new SectionFade('main', {
+        menu: '#menu',
+        sectionSelector: '.sf',
+        includeAnchor: true
+    });
+
+   //Circle following mouse
+    $(document).mousemove(function (e) {
+        $('.pointer').css({ left: e.pageX, top: e.pageY });
+    })
 
     //Ripple effect
     $('#home').ripples({
-        dropRadius: 80,
+        dropRadius: 90,
         perturbance: 0.04,
     });
-
-    //Circle following mouse
-    $(document).mousemove(function(e){
-        $('.pointer').css({left:e.pageX, top:e.pageY});
-    })
-
-})();
+});
