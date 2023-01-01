65e42d1c27f07b293e12e8f120fa68358026eb08
leijendary <jonathanleijendekker@gmail.com>
2023-01-02 04:18:57 +0800

    new tab when clicking on works

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -12,6 +12,7 @@ const Work = (props: WorkProps) => {
     <a
       className={`group m-4 overflow-hidden relative w-[calc(50% - 32px)] z-20 h-80 ${cursor}`}
       href={props.disabled ? 'javascript:;' : `/works/${props.name}`}
+      target='_blank'
     >
       <div className='lg:group-hover:left-0 lg:group-hover:opacity-100 bg-inverse-surface flex text-background flex-col min-h-min lg:h-full justify-center bottom-0 left-0 lg:-left-full opacity-100 lg:opacity-0 absolute w-full z-10 transition-all duration-200 py-6 px-4 md:px-12'>
         <div className='font-semibold mb-4 uppercase tracking-[0.01em] text-xs'>
